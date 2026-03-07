ba46f4755060f8fbb3f3aa50f1d2bb638c13161d
Paul Deon Foli <astrosteck@gmail.com>
2026-03-07 09:24:26 -0800

    Created the ask episilion page, add, search input , and some sample messages

--- Episilion-hostels/src/AskEpisilionPage/AskEpisilionPage.jsx
+++ Episilion-hostels/src/AskEpisilionPage/AskEpisilionPage.jsx
@@ -1,25 +1,65 @@
 import { PageHeader } from "../PageHeader/PageHeader";
 import { Link } from "react-router-dom";
 import { SiteFooter } from "../SiteFooter/SiteFooter";
+import './AskEpisilion.css'
+import { useState } from "react";
 
 
 
 
 
 
+export function AskEpisilionPage({ navlink, setNavLink }) {
+    const [userSearchInput, setUserSearchInput] = useState('');
+    const [userMessage, setUserMessage] = useState('This is the user message');
+    const [episilionMessage, setEpisilionMessage] = useState('This is ask epislion message');
+    const [displayMessage, setDisplayMessage] = useState('')
 
-export function AskEpisilionPage({ navlink, setNavLink}){
+    function searchInput(event) {
+        const result = event.target.value;
+        console.log(result)
+        console.log(result)
+        setUserSearchInput(result)
+
+    }
+
+    function sendMessage(){
+        setUserMessage(userSearchInput);
+        setUserSearchInput('')
+    }
 
 
 
-    return(
+    return (
         <>
             <PageHeader navlink={navlink} setNavLink={setNavLink} substituteLink={<Link className="link" to="/">Home</Link>} />
 
 
-            <SiteFooter/>
+            <div className="messages">
+                <div className="user-message">
+                    {userMessage}
+                    
+                </div>
+
+                <div className="episilion-message">
+                    {episilionMessage}
+                </div>
+            </div>
+
+
+            <div className="ask-episilion-search">
+                <input
+                    type="text"
+                    className="ask-episilion-input-box"
+                    onChange={searchInput}
+                    value={userSearchInput}
+                />
+                <button className="ask-episilion-search-button" onClick={sendMessage}>Search</button>
+            </div>
+
+            <SiteFooter />
         </>
-        
+
 
     )
 }
