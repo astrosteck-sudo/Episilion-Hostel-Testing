d2306792ae275a59a2d6f2b5be1416ef136cd2c6
Paul Deon Foli <astrosteck@gmail.com>
2026-04-11 14:43:45 -0700

    Installed cron to delete any review that is more than 2 months old

--- Episilion_Backend/db.js
+++ Episilion_Backend/db.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 const mysql = require("mysql2");
 
 
@@ -7,6 +8,26 @@ const db = mysql.createConnection({
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD, // your MySQL password
   database: process.env.DB_NAME ||  "mydb"
+});
+
+
+//THIS CODE RUNS EVERY MIDNIGHT TO DELETE ANY REVIEW THAT IS MORE THAN 2 MONTHS OLD
+const cron = require("node-cron")
+cron.schedule("0 0 * * *", () => {
+  console.log("Running cleanup job...");
+
+  const sql = `
+    DELETE FROM Reviews
+    WHERE created_at <= NOW() - INTERVAL 2 MONTH
+  `;
+
+  db.query(sql, (err, result) => {
+    if (err) {
+      console.log("Error deleting old reviews:", err);
+    } else {
+      console.log("Deleted rows:", result.affectedRows);
+    }
+  });
 });
 
 db.connect((err) => {
